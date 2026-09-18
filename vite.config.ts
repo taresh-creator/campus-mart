a11dbede47abf936fffba62777b5f6b02752f411
Taresh Ivalekar <tareshivalekar@gmail.com>
2026-09-18 20:46:26 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/campus-mart/',
   plugins: [react()],
   resolve: {
     alias: {
